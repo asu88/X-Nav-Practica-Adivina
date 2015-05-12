b4fae8500d9b47a07a9763909cf8228d3554982e
asu88 <asu.ramon88@gmail.com>
2015-05-12 19:42:18 +0200

    añadidos los botones y programados sus eventos

--- js/game.js
+++ js/game.js
@@ -7,9 +7,44 @@ var map = L.map('map', {
 });
 
 
- L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+/* L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     maxZoom: 18
+}).addTo(map); visto como geográfica */
+
+/* L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.{ext}', {
+	type: 'sat',
+	ext: 'jpg',
+	attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency',
+	subdomains: '1234'
+}).addTo(map);  Visto como tierra */
+/*
+L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/normal.day.grey/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', {
+	attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
+	subdomains: '1234',
+	mapID: 'newest',
+	app_id: 'Y8m9dK2brESDPGJPdrvs',
+	app_code: 'dq2MYIvjAotR8tHvY8Q_Dg',
+	base: 'base',
+	maxZoom: 20
+}).addTo(map); */
+
+
+/* L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.{ext}', {
+	type: 'map',
+	ext: 'jpg',
+	attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: '1234'
+}).addTo(map); */
+
+/* L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map); */
+
+
+L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+	maxZoom: 16
 }).addTo(map);
 
 
@@ -25,7 +60,7 @@ var map = L.map('map', {
                 .done(function( data ) {
                     $.each( data.items, function( i, item ) {
                     $( "<br><img>" ).attr( "src", item.media.m ).appendTo( "#images" );
-                    if ( i === 5 ) {
+                    if ( i === 4 ) {
                         return false;
                 }
 
@@ -34,15 +69,37 @@ var map = L.map('map', {
             
         }
       
-             
-        getImages("madrid");
+      /*  setInterval(function(){ getImages("madrid"); }, 10000);      */
+        getImages("madrid");  
 
-
-
-
+        
+        
 
 /* ZONA de CONTROLES */
+    $("#tab2").hide();   
+    var nivel = 0;
+     $("td.nivel").html("nivel "+nivel)
 
+    $("button.play").click(function() {        	    
+       // alert("el juego ha comenzado");
+        $("button.play").hide();
+     });
 
+    $("button.restart").click(function() {        	    
+       // alert("has reseteado el juego");
+        $("button.play").show();
+        //$("button.play").html("jugando");
+     });
+
+    $("button.new").click(function() {        	    
+      //  alert("creando nuevo juego");
+        
+     });
+
+    $("button.mostrar").click(function() {        	    
+       // alert("mostrando juegos");
+        $("#tab1").hide();
+        $("#tab2").show();
+     });
 
 })
